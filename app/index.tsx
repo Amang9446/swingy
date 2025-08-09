74a7bcb20530d01da36cc2b2bc13857fb169ff69
amang9446 <dev.aman7571@gmail.com>
2025-08-09 20:48:46 +0530

    feat: add image assets and constants for icons and categories

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
    <View className="flex flex-1 justify-center">
-      <Text className="text-xl font-bold text-blue-500 font-quicksand-bold">
+      <Text className="text-5xl text-primary text-center font-quicksand-bold">
         Welcome to Nativewind!
       </Text>
     </View>
